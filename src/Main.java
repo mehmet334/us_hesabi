7bb0e76a6b8c4bfc254f3f5a9111c37c455a96cc
Mehmet Ali Tezcan <mehmetalitez33@gmail.com>
2025-03-15 23:03:18 +0300

    ilk

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Bir Taban Sayı giriniz: ");
+        int tabansayi= scanner.nextInt();
+        System.out.println("Bir Üs Sayı giriniz: ");
+        int ussayi= scanner.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        double deger=ushesabi(tabansayi,ussayi);
+        System.out.println(deger);
+    }
+    private static double ushesabi(int sayi,int us){
+        if (us==0) return 1;
+        if (us>0) return sayi *ushesabi(sayi,us-1);
+        return 1.0 / ushesabi(sayi,-us);
     }
 }
